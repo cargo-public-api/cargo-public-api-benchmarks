abe1a8515f6542b3c18cfb895ae0209945d3a667
Martin Nordholts <enselic@gmail.com>
2023-06-18 15:40:30 +0200

    Add benchmarking bin

---
+++ src/main.rs
@@ -0,0 +1,7 @@
+fn main() {
+    // https://github.com/awslabs/aws-sdk-rust.git ;  cd aws-sdk-rust
+    // cargo +nightly rustdoc -p aws-sdk-ec2 -- -Zunstable-options -wjson
+    let rustdoc_json = "/home/martin/src/aws-sdk-rust/target/doc/aws_sdk_ec2.json";
+    let public_api = public_api::Builder::from_rustdoc_json(rustdoc_json).build().unwrap();
+    println!("{}", public_api);
+}
