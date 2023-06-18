d24bce9d2737837bc627c6c659f7968d462e3370
Martin Nordholts <enselic@gmail.com>
2023-06-18 17:03:03 +0200

    Do not benchmark sorting

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,9 @@ fn main() {
     // https://github.com/awslabs/aws-sdk-rust.git ;  cd aws-sdk-rust
     // cargo +nightly rustdoc -p aws-sdk-ec2 -- -Zunstable-options -wjson
     let rustdoc_json = "/home/martin/src/aws-sdk-rust/target/doc/aws_sdk_ec2.json";
-    let public_api = public_api::Builder::from_rustdoc_json(rustdoc_json).build().unwrap();
+    let public_api = public_api::Builder::from_rustdoc_json(rustdoc_json)
+        .sorted(false)
+        .build()
+        .unwrap();
     println!("{}", public_api);
 }
